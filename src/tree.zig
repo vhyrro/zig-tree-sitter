32278562bd326fba7a01a54a87170ad7d5895dee
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-08-29 16:00:38 -0400

    feat(tree): add more wrapper functions as `deinit` and `get_language`

--- src/tree.zig
+++ src/tree.zig
@@ -1,11 +1,17 @@
 //! Wrapper around the TSTree structure
 
 const api = @import("api/out.zig");
+const language = @import("language.zig").Language;
 
 /// Tree struct, equivalent of `TSTree`
 pub const Tree = struct {
     /// `TSTree` struct instance
     tree: *const api.TSTree,
+
+    /// Delete the syntax tree, freeing all of the memory that is used.
+    fn deinit(self: Tree) void {
+        api.ts_tree_delete(self.tree);
+    }
 
     /// Create a shallow copy of the syntax tree.
     ///
@@ -13,5 +19,10 @@ pub const Tree = struct {
     /// a time, as syntax trees are not thread safe.
     fn copy(self: Tree) Tree {
         return api.ts_tree_copy(self.tree);
+    }
+
+    /// Get the language that was used to parse the syntax tree.
+    fn get_language(self: Tree) Language.language {
+        return api.ts_tree_language(self.tree);
     }
 };
