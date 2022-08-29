e970d321c505935c461ffe3ecfcac9736d30b2fa
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-08-29 15:53:45 -0400

    docs: add documentation to Zig source code

--- src/symbol.zig
+++ src/symbol.zig
@@ -1,16 +1,21 @@
 const api = @import("api/out.zig");
 const language = @import("language.zig");
 
+/// Symbols types enum
 pub const SymbolType = enum(c_uint) {
     Regular = 0,
     Anonymous = 1,
     Auxiliary = 2,
 };
 
+/// Symbol struct, equivalent of `TSSymbol`
 pub const Symbol = struct {
+    /// `TSSymbol` struct instance
     symbol: *const api.TSSymbol,
+    /// Symbol type
     type: SymbolType,
 
+    /// Initialize a new Symbol
     fn init(lang: language.Language, symbol: *const api.TSSymbol) Symbol {
         return Symbol {
             .symbol = symbol,
--- src/tree.zig
+++ src/tree.zig
@@ -2,9 +2,15 @@
 
 const api = @import("api/out.zig");
 
+/// Tree struct, equivalent of `TSTree`
 pub const Tree = struct {
+    /// `TSTree` struct instance
     tree: *const api.TSTree,
 
+    /// Create a shallow copy of the syntax tree.
+    ///
+    /// You need to copy a syntax tree in order to use it on more than one thread at
+    /// a time, as syntax trees are not thread safe.
     fn copy(self: Tree) Tree {
         return api.ts_tree_copy(self.tree);
     }
