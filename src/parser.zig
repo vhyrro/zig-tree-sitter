8b4ff94936e23cb76415185b25fe1fc58daf7e91
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-08-29 15:53:27 -0400

    feat(parser): add documentation
    fix(parser): do not return a boolean on `Parser.set_language` function

--- src/parser.zig
+++ src/parser.zig
@@ -1,32 +1,42 @@
-//! Wrapper around the TSParser structure
+//! Wrapper around the TSParser structure.
 
 const api = @import("api/out.zig");
 const language = @import("language.zig").Language;
 
+/// Tree-Sitter Parser errors.
 pub const ParserError = error{IncompatibleParserVersion};
 
+/// Parser struct, equivalent of `TSParser` struct.
 pub const Parser = struct {
     parser: *const api.TSParser,
 
+    /// Create a new parser.
     pub fn new() Parser {
         return api.ts_parser_new();
     }
 
+    /// 
     fn reset(self: Parser) void {
         return api.ts_parser_reset(self.parser);
     }
 
+    /// Delete the parser, freeing all of the memory that is used.
     fn delete(self: Parser) void {
         return api.ts_parser_delete(self.parser);
     }
 
+    /// Set the language that the parser should use for parsing.
+    ///
+    /// Returns a `ParserError.IncompatibleParserVersion` error if the language
+    /// assignation failed due to a language generated with an incompatible
+    /// version of the Tree-Sitter CLI.
     fn set_language(self: Parser, lang: language.language) !void {
         var successful = api.ts_parser_set_language(self.parser, lang);
         if (!successful)
             return ParserError.IncompatibleParserVersion;
-        return successful;
     }
 
+    /// Get the parser's current language.
     fn get_language(self: Parser) language.language {
         return api.ts_parser_language(self.parser);
     }
