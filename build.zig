54bf7f5504d189aedf1917ebc23737c7878e89ed
Vhyrro <vhyrro@gmail.com>
2022-09-08 18:27:37 +0200

    fix: use absolute paths to get it to actually work as a library

--- build.zig
+++ build.zig
@@ -1,21 +1,22 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
+    const root = comptime std.fs.path.dirname(@src().file) orelse ".";
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("zig-tree-sitter", "src/lib.zig");
+    const lib = b.addStaticLibrary("zig-tree-sitter", root ++ "/src/lib.zig");
     lib.use_stage1 = true;
     lib.setBuildMode(mode);
     lib.linkLibC();
-    lib.addIncludeDir("tree-sitter/lib/src");
-    lib.addIncludeDir("tree-sitter/lib/include/");
-    lib.addCSourceFile("tree-sitter/lib/src/lib.c", &[_][]const u8 {});
+    lib.addIncludeDir(root ++ "/tree-sitter/lib/src");
+    lib.addIncludeDir(root ++ "/tree-sitter/lib/include/");
+    lib.addCSourceFile(root ++ "/tree-sitter/lib/src/lib.c", &[_][]const u8 {});
     lib.install();
 
-    var main_tests = b.addTest("src/tests.zig");
+    var main_tests = b.addTest(root ++ "/src/tests.zig");
     main_tests.linkLibrary(lib);
-    main_tests.addIncludeDir("tree-sitter-json/src/");
-    main_tests.addCSourceFile("tree-sitter-json/src/parser.c", &[_][]const u8 {});
+    main_tests.addIncludeDir(root ++ "/tree-sitter-json/src/");
+    main_tests.addCSourceFile(root ++ "/tree-sitter-json/src/parser.c", &[_][]const u8 {});
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
